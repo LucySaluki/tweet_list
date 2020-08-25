7afebb72404a6635c21471ca648e1392c96095d7
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-25 18:40:56 +0100

    MVP of tweet details avatar and total likes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,61 @@
 <template>
-  <h1>Tweet List</h1>
+  <div>
+    <h1>Tweet List</h1>
+    <h2>Total Likes: {{totalLikes}}</h2>
+    <ul>
+      <tweet-list v-for="(tweet, index) in tweets" :key="index" :tweet="tweet"></tweet-list>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import TweetListItem from './components/TweetListItem.vue';
 
+export default {
+  name: 'app',
+  data() {
+  return {
+    tweets:[
+      {
+        id: 1,
+        name: 'James',
+        handle: '@jokerjames',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: "If you don't succeed, dust yourself off and try again.",
+        likes: 10,
+      },
+      {
+        id: 2,
+        name: 'Fatima',
+        handle: '@fantasticfatima',
+        img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+        tweet: 'Better late than never but never late is better.',
+        likes: 12,
+      },
+      {
+        id: 3,
+        name: 'Xin',
+        handle: '@xeroxin',
+        img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+        tweet: 'Beauty in the struggle, ugliness in the success.',
+        likes: 18,
+      }
+    ]
+  }
+},
+  computed:{
+          totalLikes: function(){
+              return this.tweets.reduce((total, tweet) => total + tweet.likes,0);
+          }
+      },
+  components:{
+        'tweet-list':TweetListItem
+  }
 }
 </script>
 
 <style>
-
+body {
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
 </style>
---
+++ src/components/TweetListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li><img :src="tweet.img"><span> {{tweet.name}}{{tweet.handle}}</span>
+   <p>{{ tweet.tweet }}</p>
+   <span>Likes: {{tweet.likes}}</span></li>
+</template>
+
+<script>
+export default {
+    name: 'tweet-list',
+    props:['tweet'],
+}
+</script>
+
+<style>
+li{
+    list-style: none;
+    border-style:groove;
+    padding: 10px;
+    margin: 10px;
+}
+img {
+    width:10%;
+}
+</style>
